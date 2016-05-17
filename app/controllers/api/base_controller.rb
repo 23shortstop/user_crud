166168fcc3645a0f9db7f6100f51dc90b230673e
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-05-17 22:34:51 +0300

    Added a common implementation of json rendering.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,11 +1,26 @@
 class Api::BaseController < ::ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound do |e|
-    render json: { error: e.message }, status: :not_found
+    render_error(e.message, 404)
   end
 
   rescue_from ActiveRecord::RecordInvalid do |e|
-    render json: { error: e.message }, status: :unprocessable_entity
+    render_error(e.message , 422)
+  end
+
+  def render_response(data, status = 200)
+    render_json data, status
+  end
+
+  def render_error(message, status = 400)
+    data = { error: message }
+    render_json data, status
+  end
+
+  private
+
+  def render_json(data, status)
+    render json: data, status: status
   end
 
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,29 +1,29 @@
 class Api::UsersController < Api::BaseController
 
   def index
-    render json: User.all
+    render_response User.all
   end
 
   def show
-    render json: find_user
+    render_response find_user
   end
 
   def create
     user = User.new(user_params)
     user.save!
-    render json: user
+    render_response user
   end
 
   def update
     user = find_user
     user.update!(user_params)
-    render json: user
+    render_response user
   end
 
   def destroy
     user = find_user
     user.destroy
-    render json: user
+    render_response user
   end
 
   private
