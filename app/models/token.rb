8d4e5e8dc70145a41bc95e670da7a1896c26e83f
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-05-18 00:11:04 +0300

    Implemented generation of tokens.

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,2 +1,13 @@
 class Token < ActiveRecord::Base
+  before_create :generate_token
+
+  protected
+
+  def generate_token
+    self.token = loop do
+      random_token = SecureRandom.base64(20)
+      break random_token unless Token.exists?(token: random_token)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  has_many :tokens
 
   validates :name, :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
