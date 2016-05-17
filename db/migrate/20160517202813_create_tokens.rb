4bb22cc96cb824a164451a0967204b0ba0676de1
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-05-17 23:38:45 +0300

    Created the Token model.

---
+++ db/migrate/20160517202813_create_tokens.rb
@@ -0,0 +1,9 @@
+class CreateTokens < ActiveRecord::Migration
+  def change
+    create_table :tokens do |t|
+      t.belongs_to :user, index: true
+      t.string :token
+      t.timestamps null: false
+    end
+  end
+end
