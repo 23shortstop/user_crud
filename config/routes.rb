2294d5b5e1d58fdac760b1c427717c219a19cc1d
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-05-26 14:41:30 +0300

    Fix routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users, only: [:index, :create, :show, :update, :destroy]
-    resource :sessions, only: [:create, :destroy]
-    delete '/sessions', to: 'sessions#destroy'
+    resource :session, only: [:create, :destroy]
     resources :images, only: [:index, :create, :show, :update, :destroy]
   end
 
