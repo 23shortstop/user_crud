7d23ea2ba602c2f144a6e5e6c51972ed13f22506
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-06-06 18:32:03 +0300

    Fix the task factory.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,50 +3,61 @@ FactoryGirl.define do
     user   { create :user }
     image  { create :image }
 
-    factory :task_with_valid_resize_params do
-      type :resize
-      params do
-        { :width => Faker::Number.number(3),
-          :height => Faker::Number.number(3) }
-      end
-    end
-
-    sequence :tasks_with_invalid_resize_params do
-      invalid_params = [
-        nil,
-        { :width => Faker::Number.number(3) },
-        { :height => Faker::Number.number(3) },
-        { :width => -1, :height => Faker::Number.number(3) },
-        { :width => Faker::Number.number(3), :height => -1 },
-        { :width => '100', :height => Faker::Number.number(3) },
-        { :width => Faker::Number.number(3), :height => '100' }
-      ]
-
-      invalid_params.map do |p|
-        create(:task, :type => :resize, :params => p)
-      end
-    end
-
-    factory :with_valid_rotate_params do
+    factory :rotate_without_params do
       type :rotate
-      params do
-        { :angle => Faker::Number.between(0, 360) }
+
+      factory :with_valid_rotate_params do
+        params do
+          { :angle => Faker::Number.between(0, 360) }
+        end
+      end
+
+      factory :with_negative_angle do
+        params { { :angle => -1 } }
+      end
+
+      factory :with_exceeding_angle do
+        params { { :angle => 361 } }
+      end
+
+      factory :with_wrong_angle_type do
+        params { { :angle => '90' } }
       end
     end
 
-    sequence :with_invalid_rotate_params do
-      invalid_params = [
-        nil,
-        { :angle => -1 },
-        { :angle => 361 },
-        { :angle => '90' },
-      ]
+    factory :resize_without_params do
+      type :resize
 
-      invalid_params.map do |p|
-        create(:task, :type => :rotate, :params => p)
+      factory :with_valid_resize_params do
+        params do
+          { :width => Faker::Number.number(3),
+            :height => Faker::Number.number(3) }
+        end
+      end
+
+      factory :resize_without_width do
+        params { { :height => Faker::Number.number(3) } }
+      end
+
+      factory :resize_without_height do
+        params { { :width => Faker::Number.number(3) } }
+      end
+
+      factory :resize_with_wrong_width do
+        params { { :width => -1, :height => Faker::Number.number(3) } }
+      end
+
+      factory :resize_with_wrong_height do
+        params { { :width => Faker::Number.number(3), :height => -1 } }
+      end
+
+      factory :resize_with_wrong_width_type do
+        params { { :width => '100', :height => Faker::Number.number(3) } }
+      end
+
+      factory :resize_with_wrong_height_type do
+        params { { :width => Faker::Number.number(3), :height => '100' } }
       end
     end
-
   end
-
 end
