831f1c2515edd0972dcb8517258c0aee46ef1216
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-06-06 09:50:04 +0300

    Improve task tests.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,14 +3,42 @@ require "rails_helper"
 RSpec.describe Task, type: :model do
   subject { build :task }
 
-  describe 'validations' do
+  describe 'general validations' do
     it { is_expected.to validate_presence_of :type }
     it { is_expected.to validate_presence_of :status }
+    it { is_expected.to validate_presence_of :user }
+    it { is_expected.to validate_presence_of :image }
   end
 
   describe 'associations' do
     it { is_expected.to belong_to :user }
     it { is_expected.to have_one :image }
+  end
+
+  describe 'params validation' do
+    context 'resize' do
+      it 'is epect to be valid' do
+        valid_task = build(:task_with_valid_resize_params)
+        expect(valid_task).to be_valid
+      end
+
+      it 'is epect to be invalid' do
+        invalid_tasks = build(:tasks_with_ivalid_resize_params)
+        invalid_tasks.each { |t| expect(t).not_to be_valid }
+      end
+    end
+
+    context 'rotate' do
+      it 'is epect to be valid' do
+        valid_task = build(:task_with_valid_rotate_params)
+        expect(valid_task).to be_valid
+      end
+
+      it 'is epect to be invalid' do
+        invalid_tasks = build(:tasks_with_ivalid_rotate_params)
+        invalid_tasks.each { |t| expect(t).not_to be_valid }
+      end
+    end
   end
 
 end
